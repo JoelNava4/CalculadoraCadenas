e4042d4b40571f4c14f0d27884943aa9a096f94c
joel.navarro@ucb.edu.bo <joel.navarro@ucb.edu.bo>
2024-11-07 07:08:36 -0400

    Ingreso de cadena con delimitadores predeterminados

--- src/calcularCadena.js
+++ src/calcularCadena.js
@@ -1,5 +1,12 @@
 export function calcularCadena(cadena) {
     if (cadena === "") return 0;
+    let delimitadores = [',', '-']; 
+    let numeros = cadena;
+  
+    const regex = new RegExp(`[${delimitadores.join('')}]`);
+  
+    let listaNumeros = numeros.split(regex).map(Number);  
+    return listaNumeros.reduce((acc, num) => acc + num, 0);
   }
   
   
--- src/calcularCadena.spec.js
+++ src/calcularCadena.spec.js
@@ -4,4 +4,16 @@ describe('Calculadora de cadena', () => {
     it('debe devolver 0 para una cadena vacía', () => {
         expect(calcularCadena("")).toBe(0);
     });
+
+    it('debe devolver el número para una cadena con un solo número', () => {
+        expect(calcularCadena("2")).toBe(2);
+    });
+
+    it('debe devolver la suma de números separados por comas', () => {
+        expect(calcularCadena("1,2,4,8")).toBe(15);
+    });
+
+    it('debe manejar el delimitador "-" y devolver la suma', () => {
+        expect(calcularCadena("1-2-3")).toBe(6);
+    });
 });
